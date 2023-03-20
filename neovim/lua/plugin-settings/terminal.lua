d7c1dc87f9fe799ed00966fd785399b18c763cce
AsPulse <aspulse.x@gmail.com>
2023-03-20 14:12:55 +0900

    Fix Lazygit GPG Bug

--- neovim/lua/plugin-settings/terminal.lua
+++ neovim/lua/plugin-settings/terminal.lua
@@ -1,10 +1,9 @@
+local _terminal_lazygit = nil
+
 local _terminal_worker = nil
 local _terminal_worker_cwd = nil
 
 local _terminal_server = nil
-
-local _terminal_lazygit = nil
-local _terminal_lazygit_cwd = nil
 
 local function _terminal_worker_ready()
   if _terminal_worker == nil then
@@ -29,28 +28,22 @@ local function _terminal_worker_open()
   _terminal_worker:open()
 end
 
-local function _terminal_lazygit_ready()
-  if _terminal_lazygit ~= nil and _terminal_lazygit_cwd ~= vim.fn.getcwd() then
-    _terminal_lazygit_cwd = vim.fn.getcwd()
-    _terminal_lazygit:shutdown()
-    _terminal_lazygit = nil
-  end
-  if _terminal_lazygit == nil then
-    _terminal_lazygit = require('toggleterm.terminal').Terminal:new({
-      cmd = 'lazygit',
-      dir = vim.fn.getcwd(),
-      direction = 'float',
-      hidden = true,
-      count = 1,
-    })
-    _terminal_lazygit:spawn()
-    _terminal_lazygit_cwd = vim.fn.getcwd()
-  end
-end
-
 local function _terminal_lazygit_open()
-  _terminal_lazygit_ready()
+  if _terminal_lazygit ~= nil then
+    _terminal_lazygit:shutdown()
+  end
+
+  _terminal_lazygit = require('toggleterm.terminal').Terminal:new({
+    cmd = 'zsh',
+    dir = vim.fn.getcwd(),
+    direction = 'float',
+    hidden = true,
+    count = 1,
+  })
+
   _terminal_lazygit:open()
+  _terminal_lazygit:send('lazygit')
+
 end
 
 local function _terminal_server_open()
@@ -73,18 +66,9 @@ local function _terminal_close()
   if _terminal_worker ~= nil then
     _terminal_worker:close()
   end
-  if _terminal_lazygit ~= nil then
-    _terminal_lazygit:close()
-  end
   if _terminal_server ~= nil then
     _terminal_server:close()
   end
-end
-
-local function prepare()
-  require('notify')('Preparing Terminal…', 'info', { title = 'ToggleTerm', timeout = 1000 })
-  _terminal_worker_ready()
-  _terminal_lazygit_ready()
 end
 
 return {
@@ -103,13 +87,6 @@ return {
         start_in_insert = true,
         persist_mode = false,
       })
-
-      prepare()
-
-      vim.api.nvim_create_autocmd({ 'DirChanged' }, {
-        callback = prepare
-      })
-
     end,
   },
 }
