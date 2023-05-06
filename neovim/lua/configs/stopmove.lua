2ccd207b0537906ca43cd863f4aa6cde4bf552ff
AsPulse <aspulse.x@gmail.com>
2023-05-06 10:50:55 +0900

    Add Jump Move With Control

---
+++ neovim/lua/configs/stopmove.lua
@@ -0,0 +1,4 @@
+vim.keymap.set('n', '<C-h>', '^', { silent = true, remap = true })
+vim.keymap.set('n', '<C-l>', '$', { silent = true, remap = true })
+vim.keymap.set('n', '<C-j>', 'G', { silent = true, remap = true })
+vim.keymap.set('n', '<C-k>', 'gg', { silent = true, remap = true })
