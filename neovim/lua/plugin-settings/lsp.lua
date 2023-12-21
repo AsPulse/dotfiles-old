37bed4a699fdbddc17b2317b9bbecabe8c078149
AsPulse <aspulse.x@gmail.com>
2023-12-21 19:54:58 +0900

    [nvim] remove unused function

--- neovim/lua/plugin-settings/lsp.lua
+++ neovim/lua/plugin-settings/lsp.lua
@@ -1,7 +1,3 @@
-function file_exists(name)
-   local f=io.open(name,'r')
-   if f ~= nil then io.close(f) return true else return false end
-end
 return {
   {
     'williamboman/mason-lspconfig.nvim',
