9eb2aa4bb53728911242d0458f38aa4964c6e606
AsPulse / あすぱる <contact@aspulse.dev>
2024-02-04 02:30:16 +0900

    [arch] add desktops

---
+++ hypr/rofi.sh
@@ -0,0 +1,1 @@
+rofi -show drun -theme ~/.config/rofi/launchers/type-7/style-6.rasi -run-command '/home/aspulse/repos/dotfiles/launch-with-custom-argument/target/release/launch-with-custom-argument {cmd}' 
